873e8d3248c66e0c743061da1dccebeb0dd0da34
Gaurav Katiyar <gkyar@email.com>
2022-03-27 13:14:07 +0100

    Sample App

---
+++ src/main/java/com/securityheaders/demo/SecurityHeadersCheckerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.securityheaders.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHeadersCheckerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHeadersCheckerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securityheaders/demo/SecurityHeadersCheckerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityheaders.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHeadersCheckerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
